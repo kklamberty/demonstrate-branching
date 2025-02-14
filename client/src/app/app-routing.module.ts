6ff34136f4a736d3047514f76cadd96769d90195
K.K. Lamberty <lamberty@morris.umn.edu>
2025-02-14 15:47:00 -0600

    Add route for `users`

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { UserListComponent } from './users/user-list/user-list.component';
 
 // Note that the 'users/new' route needs to come before 'users/:id'.
 // If 'users/:id' came first, it would accidentally catch requests to
 // 'users/new'; the router would just think that the string 'new' is a user ID.
 const routes: Routes = [
   {path: '', component: HomeComponent, title: 'Home'},
+  {path: 'users', component: UserListComponent, title: 'Users'},
 ];
 
 @NgModule({
