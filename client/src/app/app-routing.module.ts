2734f4e886e490cff5c63bb0d919db3161907144
K.K. Lamberty <lamberty@morris.umn.edu>
2025-02-14 15:00:35 -0600

    Remove almost everything
    I removed almost all of the code and tests. The testing coverage of what remains is quite high.

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { AddUserComponent } from './users/add-user.component';
-import { UserListComponent } from './users/user-list.component';
-import { UserProfileComponent } from './users/user-profile.component';
-import { CompanyListComponent } from './company-list/company-list.component';
 
 // Note that the 'users/new' route needs to come before 'users/:id'.
 // If 'users/:id' came first, it would accidentally catch requests to
@@ -12,9 +8,6 @@ import { CompanyListComponent } from './company-list/company-list.component';
 const routes: Routes = [
   {path: '', component: HomeComponent, title: 'Home'},
   {path: 'users', component: UserListComponent, title: 'Users'},
-  {path: 'users/new', component: AddUserComponent, title: 'Add User'},
-  {path: 'users/:id', component: UserProfileComponent, title: 'User Profile'},
-  {path: 'companies', component: CompanyListComponent, title: 'Companies'}
 ];
 
 @NgModule({
