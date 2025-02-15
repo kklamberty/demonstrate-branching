fa0cd10bdf0a40a159fe5ffc1e51e410f89941d8
K.K. Lamberty <lamberty@morris.umn.edu>
2025-02-15 15:19:19 -0600

    Add mechanisms to filter by age
    The request for which age comes from the client, and the server gives back users of the correct age.

--- client/src/app/users/user-list/user-list.component.ts
+++ client/src/app/users/user-list/user-list.component.ts
@@ -1,7 +1,10 @@
-import { Component, Signal } from '@angular/core';
+import { Component, computed, signal, Signal } from '@angular/core';
 import { UserService } from '../user.service';
 import { User } from '../user';
-import { toSignal } from '@angular/core/rxjs-interop';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
+import { catchError, combineLatest, of, switchMap, tap } from 'rxjs';
+import { MatFormField, MatHint, MatInputModule } from '@angular/material/input';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-user-list',
@@ -10,6 +13,10 @@ import { toSignal } from '@angular/core/rxjs-interop';
   styleUrl: './user-list.component.scss'
 })
 export class UserListComponent {
+
+userAge = signal<number | undefined>(undefined);
+errMsg = signal<string | undefined>(undefined);
+
 /**
   * This constructor injects instance of `UserService`
   * into this component.
@@ -17,10 +24,74 @@ export class UserListComponent {
   *
   * @param userService the `UserService` used to get users from the server
   */
-  constructor(private userService: UserService) {
+  constructor(private userService: UserService, private snackBar: MatSnackBar) {
     // Nothing here – everything is in the injection parameters.
   }
 
-  users: Signal<User[]> = toSignal(this.userService.getUsers());
 
+  // The `Observable`s used in the definition of `serverFilteredUsers` below need
+  // observables to react to, i.e., they need to know what kinds of changes to respond to.
+  // We want to do the age and role filtering on the server side, so if either of those
+  // text fields change we want to re-run the filtering. That means we have to convert both
+  // of those _signals_ to _observables_ using `toObservable()`. Those are then used in the
+  // definition of `serverFilteredUsers` below to trigger updates to the `Observable` there.
+  private userAge$ = toObservable(this.userAge);
+
+  // We ultimately `toSignal` this to be able to access it synchronously, but we do all the RXJS operations
+  // "inside" the `toSignal()` call processing and transforming the observables there.
+  serverFilteredUsers =
+    // This `combineLatest` call takes the most recent values from these two observables (both built from
+    // signals as described above) and passes them into the following `.pipe()` call. If either of the
+    // `userRole` or `userAge` signals change (because their text fields get updated), then that will trigger
+    // the corresponding `userRole$` and/or `userAge$` observables to change, which will cause `combineLatest()`
+    // to send a new pair down the pipe.
+    toSignal(
+      combineLatest([this.userAge$]).pipe(
+        // `switchMap` maps from one observable to another. In this case, we're taking `role` and `age` and passing
+        // them as arguments to `userService.getUsers()`, which then returns a new observable that contains the
+        // results.
+        switchMap(([ age ]) =>
+          this.userService.getUsers({
+            age,
+          })
+        ),
+        // `catchError` is used to handle errors that might occur in the pipeline. In this case `userService.getUsers()`
+        // can return errors if, for example, the server is down or returns an error. This catches those errors, and
+        // sets the `errMsg` signal, which allows error messages to be displayed.
+        catchError((err) => {
+          if (err.error instanceof ErrorEvent) {
+            this.errMsg.set(
+              `Problem in the client – Error: ${err.error.message}`
+            );
+          } else {
+            this.errMsg.set(
+              `Problem contacting the server – Error Code: ${err.status}\nMessage: ${err.message}`
+            );
+          }
+          this.snackBar.open(this.errMsg(), 'OK', { duration: 6000 });
+          // `catchError` needs to return the same type. `of` makes an observable of the same type, and makes the array still empty
+          return of<User[]>([]);
+        }),
+        // Tap allows you to perform side effects if necessary
+        tap(() => {
+          // A common side effect is printing to the console.
+          // You don't want to leave code like this in the
+          // production system, but it can be useful in debugging.
+          console.log('Users were filtered on the server')
+        })
+      )
+    );
+
+  // No need for fancy RXJS stuff. We do the fancy RXJS stuff where we call `toSignal`, i.e., up in
+  // the definition of `serverFilteredUsers` above.
+  // `computed()` takes the value of one or more signals (`serverFilteredUsers` in this case) and
+  // _computes_ the value of a new signal (`filteredUsers`). Angular recognizes when any signals
+  // in the function passed to `computed()` change, and will then call that function to generate
+  // the new value of the computed signal.
+  // In this case, whenever `serverFilteredUsers` changes (e.g., because we change `userName`), then `filteredUsers`
+  // will be updated by rerunning the function we're passing to `computed()`.
+  filteredUsers = computed(() => {
+    const serverFilteredUsers = this.serverFilteredUsers();
+    return this.userService.filterUsers(serverFilteredUsers, {});
+  });
 }
--- server/src/test/java/umm3601/user/UserControllerSpec.java
+++ server/src/test/java/umm3601/user/UserControllerSpec.java
@@ -1,6 +1,8 @@
 package umm3601.user;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
@@ -11,8 +13,10 @@ import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.bson.Document;
 import org.bson.types.ObjectId;
@@ -36,6 +40,9 @@ import com.mongodb.client.MongoDatabase;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
 import io.javalin.http.HttpStatus;
+import io.javalin.validation.Validation;
+import io.javalin.validation.ValidationException;
+import io.javalin.validation.Validator;
 
 /**
  * Tests the logic of the UserController
@@ -196,6 +203,214 @@ class UserControllerSpec {
     assertEquals(
         db.getCollection("users").countDocuments(),
         userArrayListCaptor.getValue().size());
+  }
+
+    /**
+   * Confirm that if we process a request for users with age 37,
+   * that all returned users have that age, and we get the correct
+   * number of users.
+   *
+   * The structure of this test is:
+   *
+   *    - We create a `Map` for the request's `queryParams`, that
+   *      contains a single entry, mapping the `AGE_KEY` to the
+   *      target value ("37"). This "tells" our `UserController`
+   *      that we want all the `User`s that have age 37.
+   *    - We create a validator that confirms that the code
+   *      we're testing calls `ctx.queryParamsAsClass("age", Integer.class)`,
+   *      i.e., it asks for the value in the query param map
+   *      associated with the key `"age"`, interpreted as an Integer.
+   *      That call needs to return a value of type `Validator<Integer>`
+   *      that will succeed and return the (integer) value `37` associated
+   *      with the (`String`) parameter value `"37"`.
+   *    - We then call `userController.getUsers(ctx)` to run the code
+   *      being tested with the constructed context `ctx`.
+   *    - We also use the `userListArrayCaptor` (defined above)
+   *      to capture the `ArrayList<User>` that the code under test
+   *      passes to `ctx.json(…)`. We can then confirm that the
+   *      correct list of users (i.e., all the users with age 37)
+   *      is passed in to be returned in the context.
+   *    - Now we can use a variety of assertions to confirm that
+   *      the code under test did the "right" thing:
+   *       - Confirm that the list of users has length 2
+   *       - Confirm that each user in the list has age 37
+   *       - Confirm that their names are "Jamie" and "Pat"
+   *
+   * @throws IOException
+   */
+  @Test
+  void canGetUsersWithAge37() throws IOException {
+    // We'll need both `String` and `Integer` representations of
+    // the target age, so I'm defining both here.
+    Integer targetAge = 37;
+    String targetAgeString = targetAge.toString();
+
+    // Create a `Map` for the `queryParams` that will "return" the string
+    // "37" if you ask for the value associated with the `AGE_KEY`.
+    Map<String, List<String>> queryParams = new HashMap<>();
+
+    queryParams.put(UserController.AGE_KEY, Arrays.asList(new String[] {targetAgeString}));
+    // When the code being tested calls `ctx.queryParamMap()` return the
+    // the `queryParams` map we just built.
+    when(ctx.queryParamMap()).thenReturn(queryParams);
+    // When the code being tested calls `ctx.queryParam(AGE_KEY)` return the
+    // `targetAgeString`.
+    when(ctx.queryParam(UserController.AGE_KEY)).thenReturn(targetAgeString);
+
+    // Create a validator that confirms that when we ask for the value associated with
+    // `AGE_KEY` _as an integer_, we get back the integer value 37.
+    Validation validation = new Validation();
+    // The `AGE_KEY` should be name of the key whose value is being validated.
+    // You can actually put whatever you want here, because it's only used in the generation
+    // of testing error reports, but using the actually key value will make those reports more informative.
+    Validator<Integer> validator = validation.validator(UserController.AGE_KEY, Integer.class, targetAgeString);
+    // When the code being tested calls `ctx.queryParamAsClass("age", Integer.class)`
+    // we'll return the `Validator` we just constructed.
+    when(ctx.queryParamAsClass(UserController.AGE_KEY, Integer.class))
+        .thenReturn(validator);
+
+    userController.getUsers(ctx);
+
+    // Confirm that the code being tested calls `ctx.json(…)`, and capture whatever
+    // is passed in as the argument when `ctx.json()` is called.
+    verify(ctx).json(userArrayListCaptor.capture());
+    // Confirm that the code under test calls `ctx.status(HttpStatus.OK)` is called.
+    verify(ctx).status(HttpStatus.OK);
+
+    // Confirm that we get back two users.
+    assertEquals(2, userArrayListCaptor.getValue().size());
+    // Confirm that both users have age 37.
+    for (User user : userArrayListCaptor.getValue()) {
+      assertEquals(targetAge, user.age);
+    }
+    // Generate a list of the names of the returned users.
+    List<String> names = userArrayListCaptor.getValue().stream().map(user -> user.name).collect(Collectors.toList());
+    // Confirm that the returned `names` contain the two names of the
+    // 37-year-olds.
+    assertTrue(names.contains("Jamie"));
+    assertTrue(names.contains("Pat"));
+  }
+
+  /**
+   * Test that if the user sends a request with an illegal value in
+   * the age field (i.e., something that can't be parsed to a number)
+   * we get a reasonable error back.
+   */
+  @Test
+  void respondsAppropriatelyToNonNumericAge() {
+    Map<String, List<String>> queryParams = new HashMap<>();
+    String illegalIntegerString = "bad integer string";
+    queryParams.put(UserController.AGE_KEY, Arrays.asList(new String[] {illegalIntegerString}));
+    when(ctx.queryParamMap()).thenReturn(queryParams);
+    // When the code being tested calls `ctx.queryParam(AGE_KEY)` return the
+    // `illegalIntegerString`.
+    when(ctx.queryParam(UserController.AGE_KEY)).thenReturn(illegalIntegerString);
+
+    // Create a validator that confirms that when we ask for the value associated with
+    // `AGE_KEY` _as an integer_, we get back the `illegalIntegerString`.
+    Validation validation = new Validation();
+    // The `AGE_KEY` should be name of the key whose value is being validated.
+    // You can actually put whatever you want here, because it's only used in the generation
+    // of testing error reports, but using the actually key value will make those reports more informative.
+    Validator<Integer> validator = validation.validator(UserController.AGE_KEY, Integer.class, illegalIntegerString);
+    when(ctx.queryParamAsClass(UserController.AGE_KEY, Integer.class)).thenReturn(validator);
+
+    // This should now throw a `ValidationException` because
+    // our request has an age that can't be parsed to a number.
+    ValidationException exception = assertThrows(ValidationException.class, () -> {
+      userController.getUsers(ctx);
+    });
+    // This digs into the returned `ValidationException` to get the underlying `Exception` that caused
+    // the validation to fail:
+    //   - `exception.getErrors` returns a `Map` that maps keys (like `AGE_KEY`) to lists of
+    //      validation errors for that key
+    //   - `.get(AGE_KEY)` returns a list of all the validation errors associated with `AGE_KEY`
+    //   - `.get(0)` assumes that the root cause is the first error in the list. In our case there
+    //     is only one root cause,
+    //     so that's safe, but you might be careful about that assumption in other contexts.
+    //   - `.exception()` gets the actually `Exception` value that was the underlying cause
+    Exception exceptionCause = exception.getErrors().get(UserController.AGE_KEY).get(0).exception();
+    // The cause should have been a `NumberFormatException` (what is thrown when we try to parse "bad" as an integer).
+    assertEquals(NumberFormatException.class, exceptionCause.getClass());
+    // The message for that `NumberFOrmatException` should include the text it tried to parse as an integer,
+    // i.e., `"bad integer string"`.
+    assertTrue(exceptionCause.getMessage().contains(illegalIntegerString));
+  }
+
+  /**
+   * Test that if the user sends a request with an illegal value in
+   * the age field (i.e., too big of a number)
+   * we get a reasonable error code back.
+   */
+  @Test
+  void respondsAppropriatelyToTooLargeNumberAge() {
+    Map<String, List<String>> queryParams = new HashMap<>();
+    String overlyLargeAgeString = "151";
+    queryParams.put(UserController.AGE_KEY, Arrays.asList(new String[] {overlyLargeAgeString}));
+    when(ctx.queryParamMap()).thenReturn(queryParams);
+    // When the code being tested calls `ctx.queryParam(AGE_KEY)` return the
+    // `overlyLargeAgeString`.
+    when(ctx.queryParam(UserController.AGE_KEY)).thenReturn(overlyLargeAgeString);
+
+    // Create a validator that confirms that when we ask for the value associated with
+    // `AGE_KEY` _as an integer_, we get back the integer value 37.
+    Validation validation = new Validation();
+    // The `AGE_KEY` should be name of the key whose value is being validated.
+    // You can actually put whatever you want here, because it's only used in the generation
+    // of testing error reports, but using the actually key value will make those reports more informative.
+    Validator<Integer> validator = validation.validator(UserController.AGE_KEY, Integer.class, overlyLargeAgeString);
+    when(ctx.queryParamAsClass(UserController.AGE_KEY, Integer.class)).thenReturn(validator);
+
+    // This should now throw a `ValidationException` because
+    // our request has an age that is larger than 150, which isn't allowed.
+    ValidationException exception = assertThrows(ValidationException.class, () -> {
+      userController.getUsers(ctx);
+    });
+    // This `ValidationException` was caused by a custom check, so we just get the message from the first
+    // error and confirm that it contains the problematic string, since that would be useful information
+    // for someone trying to debug a case where this validation fails.
+    String exceptionMessage = exception.getErrors().get(UserController.AGE_KEY).get(0).getMessage();
+    // The message should be the message from our code under test, which should include the text we
+    // tried to parse as an age, namely "151".
+    assertTrue(exceptionMessage.contains(overlyLargeAgeString));
+  }
+
+  /**
+   * Test that if the user sends a request with an illegal value in
+   * the age field (i.e., too small of a number)
+   * we get a reasonable error code back.
+   */
+  @Test
+  void respondsAppropriatelyToTooSmallNumberAge() {
+    Map<String, List<String>> queryParams = new HashMap<>();
+    String negativeAgeString = "-1";
+    queryParams.put(UserController.AGE_KEY, Arrays.asList(new String[] {negativeAgeString}));
+    when(ctx.queryParamMap()).thenReturn(queryParams);
+    // When the code being tested calls `ctx.queryParam(AGE_KEY)` return the
+    // `negativeAgeString`.
+    when(ctx.queryParam(UserController.AGE_KEY)).thenReturn(negativeAgeString);
+
+    // Create a validator that confirms that when we ask for the value associated with
+    // `AGE_KEY` _as an integer_, we get back the string value `negativeAgeString`.
+    Validation validation = new Validation();
+    // The `AGE_KEY` should be name of the key whose value is being validated.
+    // You can actually put whatever you want here, because it's only used in the generation
+    // of testing error reports, but using the actually key value will make those reports more informative.
+    Validator<Integer> validator = validation.validator(UserController.AGE_KEY, Integer.class, negativeAgeString);
+    when(ctx.queryParamAsClass(UserController.AGE_KEY, Integer.class)).thenReturn(validator);
+
+    // This should now throw a `ValidationException` because
+    // our request has an age that is larger than 150, which isn't allowed.
+    ValidationException exception = assertThrows(ValidationException.class, () -> {
+      userController.getUsers(ctx);
+    });
+    // This `ValidationException` was caused by a custom check, so we just get the message from the first
+    // error and confirm that it contains the problematic string, since that would be useful information
+    // for someone trying to debug a case where this validation fails.
+    String exceptionMessage = exception.getErrors().get(UserController.AGE_KEY).get(0).getMessage();
+    // The message should be the message from our code under test, which should include the text we
+    // tried to parse as an age, namely "-1".
+    assertTrue(exceptionMessage.contains(negativeAgeString));
   }
 
   /**
