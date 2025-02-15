613e44ce7a61559a3e96582fca5051a28b44d837
K.K. Lamberty <lamberty@morris.umn.edu>
2025-02-14 18:23:06 -0600

    Make user-list show the users somehow
    This commit includes component testing, the template, and the component itself

--- client/src/app/users/user-list/user-list.component.ts
+++ client/src/app/users/user-list/user-list.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Signal } from '@angular/core';
+import { UserService } from '../user.service';
+import { User } from '../user';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-user-list',
@@ -7,5 +10,17 @@ import { Component } from '@angular/core';
   styleUrl: './user-list.component.scss'
 })
 export class UserListComponent {
+/**
+  * This constructor injects instance of `UserService`
+  * into this component.
+  * `UserService` lets us interact with the server.
+  *
+  * @param userService the `UserService` used to get users from the server
+  */
+  constructor(private userService: UserService) {
+    // Nothing here – everything is in the injection parameters.
+  }
+
+  users: Signal<User[]> = toSignal(this.userService.getUsers());
 
 }
